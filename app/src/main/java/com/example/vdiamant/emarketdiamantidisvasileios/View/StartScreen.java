23f467e71b8c3fb341a03e70817a420a91ba7e9b
vdiamantGR <basdiamantidis@gmail.com>
2017-05-10 11:32:23 +0300

    Add comments on methods

--- app/src/main/java/com/example/vdiamant/emarketdiamantidisvasileios/View/StartScreen.java
+++ app/src/main/java/com/example/vdiamant/emarketdiamantidisvasileios/View/StartScreen.java
@@ -23,6 +23,12 @@ public class StartScreen extends AppCompatActivity implements FragHome.FragHomeI
 
     }
 
+
+    //With initializeFragment() we replace the FrameLayout with the home Fragment
+    //So that the user can navigate to Login Or Register
+    //The reason we do that is because if we would have started the activity with the fragment from xml
+    //we couldn't change it later so that the FragHome or FragRegister would have been in the position of the FrameLayout
+
     private void initializeFragment() {
         Fragment homeFragment=new FragHome();
         FragmentManager fragmentManager = getFragmentManager();
@@ -33,6 +39,13 @@ public class StartScreen extends AppCompatActivity implements FragHome.FragHomeI
         fragmentTransaction.commit();
     }
 
+
+    //the method change Fragment changes the fragment of the StartScreen Activity
+    //if it's True the new Fragment is the login Fragment, if it's false it is the register fragment.
+
+
+
+    //Important! the Home Fragment stays in the back stack so if the user presses back it returns to the Home Fragment!
 
     @Override
     public void changeFragment(boolean loginOrRegister) {
@@ -53,6 +66,9 @@ public class StartScreen extends AppCompatActivity implements FragHome.FragHomeI
         fragmentTransaction.commit();
     }
 
+
+    //this method is to empty the back stack so that if the user presses back when he is at the home fragment
+    //he will exit app. Without this method he would be seeing the activity with the framelayout empty.
     private void clearBackStack(){
         FragmentManager fm = getFragmentManager();
         for(int i = 0; i < fm.getBackStackEntryCount(); ++i) {
