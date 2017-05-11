00c8d3f670b73f0d6e80f8f4e63f156b392d4f62
vdiamantGR <basdiamantidis@gmail.com>
2017-05-11 12:59:06 +0300

    Add BackgroundTask.java that controls the connection with the database for login and restister Add int FragRegister the method to do the registration to the local database through php

--- app/src/main/java/com/example/vdiamant/emarketdiamantidisvasileios/Fragments/FragRegister.java
+++ app/src/main/java/com/example/vdiamant/emarketdiamantidisvasileios/Fragments/FragRegister.java
@@ -1,11 +1,16 @@
 package com.example.vdiamant.emarketdiamantidisvasileios.Fragments;
 
+import com.example.vdiamant.emarketdiamantidisvasileios.Controller.BackgroundTask;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.vdiamant.emarketdiamantidisvasileios.Controller.BackgroundTask;
 import com.example.vdiamant.emarketdiamantidisvasileios.R;
 
 /**
@@ -13,10 +18,51 @@ import com.example.vdiamant.emarketdiamantidisvasileios.R;
  */
 
 public class FragRegister extends android.app.Fragment {
+
+    EditText etFirst,etLast,etMail,etPass,etRepass;
+    String fname,lname,userMail,userPass,userReType;
+    Button btnRegister;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.register,container,false);
+
+        etFirst=(EditText)view.findViewById(R.id.ETregfirst);
+        etLast=(EditText)view.findViewById(R.id.ETreglast);
+        etMail=(EditText)view.findViewById(R.id.ETregmail);
+        etPass=(EditText)view.findViewById(R.id.ETregPass);
+        etRepass=(EditText)view.findViewById(R.id.ETregRetypePass);
+
+        btnRegister=(Button)view.findViewById(R.id.btnregRegister);
+
+        btnRegister.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        userRegistration();
+                    }
+                }
+        );
+
+
         return view;
     }
+
+
+    public void userRegistration(){
+        fname=etFirst.getText().toString();
+        lname=etLast.getText().toString();
+        userMail=etMail.getText().toString();
+        userPass=etPass.getText().toString();
+        userReType=etRepass.getText().toString();
+
+        String method="register";
+        //edw isws einai lathos to getActivity!!
+        BackgroundTask backgroundTask=new BackgroundTask(this.getActivity());
+        backgroundTask.execute(method,fname,lname,userMail,userPass);
+
+    }
+
+
+
+
 }
